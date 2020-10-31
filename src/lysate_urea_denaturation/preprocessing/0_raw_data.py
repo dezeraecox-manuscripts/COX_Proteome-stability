0fd8226ffc54ab4d10ea2ff6fa393fda6d3074f2
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2020-10-31 15:16:31 +1100

    Added script for donloading lysate raw data

---
+++ src/lysate_urea_denaturation/preprocessing/0_raw_data.py
@@ -0,0 +1,29 @@
+import os, re
+import zipfile
+from shutil import copyfile
+from loguru import logger
+
+from utilities.database_collection import download_resources
+from utilities.database_map_and_filter import gz_unzipper, tar_file_to_folder
+
+logger.info('Import OK')
+
+
+if __name__ == "__main__":
+
+    url = 'https://' # update to repository address
+    folder_name = 'lysate_urea_denaturation/'
+    output_folder = 'raw_data/'
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Download file from repository
+    # download_resources(filename=f'{folder_name}.zip', url=url, resource_folder=output_folder) 
+    # with zipfile.ZipFile(f'{output_folder}{folder_name}.zip', 'r') as zip_ref:
+    #     zip_ref.extractall(f'{output_folder}{folder_name}')
+
+
+    
+
+
