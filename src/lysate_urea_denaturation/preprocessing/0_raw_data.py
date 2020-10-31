ab9f1e7fad12075887747597e9b8d71ee48dd4f2
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2020-10-31 15:59:58 +1100

    Add database download to raw data

--- src/lysate_urea_denaturation/preprocessing/0_raw_data.py
+++ src/lysate_urea_denaturation/preprocessing/0_raw_data.py
@@ -3,7 +3,7 @@ import zipfile
 from shutil import copyfile
 from loguru import logger
 
-from utilities.database_collection import download_resources
+from utilities import database_collection
 from utilities.database_map_and_filter import gz_unzipper, tar_file_to_folder
 
 logger.info('Import OK')
@@ -13,17 +13,18 @@ if __name__ == "__main__":
 
     url = 'https://' # update to repository address
     folder_name = 'lysate_urea_denaturation/'
-    output_folder = 'raw_data/'
+    output_folder = 'raw_data/lysate_urea_denaturation/'
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     # Download file from repository
-    # download_resources(filename=f'{folder_name}.zip', url=url, resource_folder=output_folder) 
+    # database_collection.download_resources(filename=f'{folder_name}.zip', url=url, resource_folder=output_folder) 
     # with zipfile.ZipFile(f'{output_folder}{folder_name}.zip', 'r') as zip_ref:
     #     zip_ref.extractall(f'{output_folder}{folder_name}')
 
-
+    # Download useful databases to resources folder
+    database_collection.main(tax_ids=['10090', '9606'], resource_folder='resources/bioinformatics_databases/', caller_id = "www.github.com/dezeraecox")
     
 
 
